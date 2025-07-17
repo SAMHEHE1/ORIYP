b24aeff402fbb3bfc970570213ddb8bb212e30cf
Samarth-1206 <pawar06samarth14@gmail.com>
2025-07-17 22:23:23 +0530

    Update Youtube.py

--- PURVIMUSIC/platforms/Youtube.py
+++ PURVIMUSIC/platforms/Youtube.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 from PURVIMUSIC.utils.database import is_on_off
-from PURVIIMUSIC.utils.formatters import time_to_seconds
+from PURVIMUSIC.utils.formatters import time_to_seconds
 
 import os
 import glob
