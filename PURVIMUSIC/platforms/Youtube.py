64e092d837fd4e53650cfcd3fcd77e468f186088
Samarth-1206 <pawar06samarth14@gmail.com>
2025-07-19 20:53:43 +0530

    Update Youtube.py

--- PURVIMUSIC/platforms/Youtube.py
+++ PURVIMUSIC/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from SONALI_MUSIC.utils.database import is_on_off
-from SONALI_MUSIC.utils.formatters import time_to_seconds
+from PURVIMUSIC.utils.database import is_on_off
+from PURVIMUSIC.utils.formatters import time_to_seconds
 
 
 
